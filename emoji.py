cd8c5864739316903e89c0598264c488950da4ea
Janusz Janowski <januszjanowski@outlook.com>
2021-12-06 11:31:07 +0100

    document recalculate method in emoji class

--- emoji.py
+++ emoji.py
@@ -13,7 +13,7 @@ class Emoji:
     growth: float
     speed: float
     level: int = field(default=0)
-    last_tick: datetime = field(default_factory=lambda: datetime.now())
+    last_tick: datetime = field(default_factory=datetime.now)
 
     @property
     def buy_price(self) -> float:
@@ -32,6 +32,18 @@ class Emoji:
         return self.production_per_second + self.start_income
 
     def recalculate(self) -> float:
+        """
+        Calculate income from last check and calculate next last check.
+        For example:
+        Last tick -> 00:00:00
+        current time -> 00:00:10
+        speed of emoji -> 3seconds
+        Compute time elapsed 10s
+        In 10s 3 times emoji generate income
+        return 3 time income
+        and move last tick to 3 times 3s -> 00:00:09
+        :return: return income from elapsed time
+        """
         current_time = datetime.now()
         delta: timedelta = current_time - self.last_tick
         tick_time_in_microseconds = timedelta(microseconds=self.speed * SECOND_IN_MICROSECONDS)
